4738a49375ff7dc97372685b5daccb6802aca852
Rameen Ali <rameen.ali.3287@gmail.com>
2026-08-12 15:59:51 +0500

    add URL shortening API enpoint

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from extensions import db
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///urls.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 from models import URL
 
 
@@ -14,6 +14,13 @@ from models import URL
 def home():
     return "URL Shortener API is running!"
 
+@app.route("/api/shorten", methods=["POST"])
+def shorten_url():
+    data = request.get_json()
 
+    return jsonify({
+        "received_url": data["url"]
+    })
+    
 if __name__ == "__main__":
     app.run(debug=True)
